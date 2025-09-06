78b23de05f954cacfddf56739de3e82717796bc8
Aziz <azizullah.masomi2020@gmail.com>
2025-09-06 13:25:12 +0200

    training parameter updated

--- config/paths.py
+++ config/paths.py
@@ -1,22 +1,29 @@
 import os
 import datetime
 
-IN_COLAB = 'google.colab' in str(get_ipython()) if hasattr(__builtins__, '__IPYTHON__') else False
+def is_colab():
+    try:
+        import google.colab
+        return True
+    except ImportError:
+        return False
+
+IN_COLAB = is_colab()
 
 if IN_COLAB:
     from google.colab import drive
     drive.mount('/content/drive')
-    BASE_PATH = '/content/drive/MyDrive/ColabFoulder'
+    BASE_PATH = '/content/drive/MyDrive/Sentiment_Analysis'
 else:
     BASE_PATH = './data'
 
 timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M")
 
 PATHS = {
-    "raw_data": os.path.join(BASE_PATH, "sampled_for_labeling_cleaned.csv"),
-    "labeled_data": os.path.join(BASE_PATH, "processed/Labeled_4K.csv"),
-    "base_model": os.path.join(BASE_PATH, "parsbert_emotion"),
-    "finetuned_model": os.path.join(BASE_PATH, f"fine_tuned_model_{timestamp}"),
+    "raw_data": os.path.join(BASE_PATH, "Data", "raw", "sampled_for_labeling_cleaned.csv"),
+    "labeled_data": os.path.join(BASE_PATH, "Data", "processed", "Labeled_4K.csv"),
+    "base_model": os.path.join(BASE_PATH, "Models", "parsbert_emotion"),
+    "finetuned_model": os.path.join(BASE_PATH, "Models", f"fine_tuned_model_{timestamp}"),
 }
 
 MODEL_CONFIG = {
