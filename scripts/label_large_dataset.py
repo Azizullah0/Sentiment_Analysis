2070e1d9fd0daf95c8f7d23696a742d3eb092f64
Aziz <azizullah.masomi2020@gmail.com>
2025-09-09 14:11:25 +0200

    data authomated labeling Updated

--- scripts/label_large_dataset.py
+++ scripts/label_large_dataset.py
@@ -12,8 +12,8 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 # 1. Automatically pick latest fine-tuned model from PATHS
 # -----------------------------
 # Load final fine-tuned model
+
 model_path = PATHS["finetuned_model"]
-tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
 tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
 model = AutoModelForSequenceClassification.from_pretrained(model_path, local_files_only=True)
 
