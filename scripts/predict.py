decf4c8bd5b7086d4bc501abe1b989323afb7191
Aziz Massomy <azizullah.masomi2020@gmail.com>
2025-12-04 08:42:44 +0100

    Update predict.py

--- scripts/predict.py
+++ scripts/predict.py
@@ -5,7 +5,7 @@ import numpy as np
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 # --- FIXED MODEL PATH ---
-MODEL_PATH = r"/content/drive/MyDrive/Sentiment_Analysis/outputs/incremental_imbalanced_20251029_1321/checkpoint-39168"
+MODEL_PATH = r"/content/drive/MyDrive/Sentiment_Analysis/outputs/incremental_imbalanced_20251026_1013/checkpoint-38776"
 
 LABEL_MAP = {
     0: "Hope",
