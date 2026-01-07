bd30f521864bdcbdc4257004e6e43ab7d0a2343a
Aziz Massomy <azizullah.masomi2020@gmail.com>
2026-01-07 11:53:29 +0100

    Update predict.py

--- scripts/predict.py
+++ scripts/predict.py
@@ -5,7 +5,7 @@ import numpy as np
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 # Update this to the folder that contains config.json/pytorch_model.bin/tokenizer.* (e.g., your saved run)
-MODEL_PATH = r"/content/drive/MyDrive/Sentiment_Analysis/outputs/no_fear_20260104_1019"
+MODEL_PATH = r"/content/drive/MyDrive/Sentiment_Analysis/outputs/incremental_imbalanced_20260106_1346/checkpoint-20034"
 
 # Label map must match the number/order of labels in the loaded model
 LABEL_MAP = {
@@ -16,6 +16,7 @@ LABEL_MAP = {
     4: "Disgust",
     5: "Sad",
     6: "Anger",
+    7: "Fear",
   
 }
 
